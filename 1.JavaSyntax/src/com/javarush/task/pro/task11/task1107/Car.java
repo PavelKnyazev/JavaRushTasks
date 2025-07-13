96603680a20fd78ac2a4229a556a631b2ec1a6e0
Pavel Knyazev <pavel.knyazev2603@gmail.com>
2025-07-13 16:13:47 +0300

    Внутренний класс (двигатель автомобиля)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task11.task1107;
+
+/* 
+Двигатель — сердце автомобиля
+*/
+
+public class Car {
+
+    Engine engine = new Engine();
+
+
+    public class Engine {
+        private boolean isRunning;
+
+        public void start() {
+            isRunning = true;
+            System.out.println("Двигатель запущен");
+        }
+
+        public void stop() {
+            isRunning = false;
+            System.out.println("Двигатель остановлен");
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.engine.stop();
+        car.engine.start();
+    }
+
+}
